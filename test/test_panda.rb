743b3756d92ef2e1bd8438edfdfc9824f287f901
Zach Bonham <zachbonham@gmail.com>
2010-12-26 10:03:26 -0600

    updating README

--- test/test_panda.rb
+++ test/test_panda.rb
@@ -4,4 +4,13 @@ class TestPanda < Test::Unit::TestCase
   should "probably rename this file and start testing for real" do
     flunk "hey buddy, you should probably rename this file and start testing for real"
   end
+  
+  should "find all projects in .NET solution file" do
+    flunk "need to include test solution for testing"
+  end
+  
+  should "find all web application projects in .NET solution file" do
+    flunk "need to include test solution for testing"
+  end
+  
 end
